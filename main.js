3faf056605a405ba368f38641d81557880a5fa3b
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2020-08-29 16:41:07 -0400

    primer commit

---
+++ main.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+var owl = $('.owl-carousel');
+owl.owlCarousel();
+// Go to the next item
+$('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.customPrevBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
+
+document.querySelector(".nav-btn").addEventListener("click", () => {
+    document.querySelector(".nav-left").classList.toggle("show");
+    document.querySelector(".nav-right").classList.toggle("show");
+})
